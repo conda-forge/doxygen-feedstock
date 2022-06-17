2b824404eb057d2aaa0b1d1beecd33c4e3444e55
Tadeu Manoel <tadeu@users.noreply.github.com>
2022-06-17 11:04:28 -0300

    Add _LIBCPP_DISABLE_AVAILABILITY to macOS builds
    To try to fix `use of undeclared identifier 'UTIME_OMIT'` compilation errors

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$(uname)" == "Darwin" ]; then
+  # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+  export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 mkdir build && cd build
 cmake ${CMAKE_ARGS} \
 	-DCMAKE_INSTALL_PREFIX=$PREFIX          \
