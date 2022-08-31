de766aa7855ddb7916d09d9c4b3ff3b0eff8db1f
Isuru Fernando <isuruf@gmail.com>
2022-08-31 05:29:07 -0500

    Fix macosx build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+if [[ "$target_platform" == osx-* ]]; then
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 mkdir build && cd build
 cmake ${CMAKE_ARGS} -LAH -G"$CMAKE_GENERATOR" \
 	-DCMAKE_PREFIX_PATH=${PREFIX} \
 	-DCMAKE_INSTALL_PREFIX=${PREFIX} \
 	-DCMAKE_BUILD_TYPE=Release              \
 	..
-make
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make tests
+
+make -j${CPU_COUNT}
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" || "${CMAKE_CROSSCOMPILING_EMULATOR}" != "" ]]; then
+  make tests
 fi
 make install
