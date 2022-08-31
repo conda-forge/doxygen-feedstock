56f68458b3af50cde3b28aa74b42811d32186f52
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2022-08-31 09:16:02 +0200

    Doxygen 1.9.4 and small build scripts modifications

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 mkdir build && cd build
-cmake ${CMAKE_ARGS} \
-	-DCMAKE_INSTALL_PREFIX=$PREFIX          \
+cmake ${CMAKE_ARGS} -LAH -G"$CMAKE_GENERATOR" \
+	-DCMAKE_PREFIX_PATH=${PREFIX} \
+	-DCMAKE_INSTALL_PREFIX=${PREFIX} \
 	-DCMAKE_BUILD_TYPE=Release              \
 	..
 make
