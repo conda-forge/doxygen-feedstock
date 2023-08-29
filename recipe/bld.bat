c3ad1e434c52b5716409bccc2149c4731e476348
Tom de Geus <tom@geus.me>
2023-08-29 10:48:25 +0200

    debug

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,54 +7,48 @@ cd build
 set BISON_PKGDATADIR=%BUILD_PREFIX%\Library\share\winflexbison\data
 
 :: debug
-echo "=== perl ===";
-perl --version;
-echo "=== python ===";
-python --version;
-echo "=== cmake ===";
-cmake --version;
-echo "=== latex ===";
-Try {
-    latex --version;
-}
-Catch {
-    echo "latex not found";
-}
-echo "=== bibtex ===";
-Try {
-    bibtex --version;
-}
-Catch {
-    echo "bibtex not found";
-}
-echo "=== dvips ===";
-Try {
-    dvips --version;
-}
-Catch {
-    echo "dvips not found";
-}
-echo "=== bison ===";
-win_bison --version;
-echo "=== flex ===";
-win_flex --version;
-echo "=== dot ===";
-dot -V;
-echo "=== ghostscript ===";
-gswin64c --version;
+echo "=== perl ==="
+perl --version
+
+echo "=== python ==="
+python --version
+
+echo "=== cmake ==="
+cmake --version
+
+echo "=== latex ==="
+latex --version
+
+echo "=== bibtex ==="
+bibtex --version
+
+echo "=== dvips ==="
+dvips --version
+
+echo "=== bison ==="
+win_bison --version
+
+echo "=== flex ==="
+win_flex --version
+
+echo "=== dot ==="
+dot -V
+
+echo "=== ghostscript ==="
+gswin64c --version
 
 :: cmake
 cmake -G "Ninja" ^
     -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_BUILD_TYPE:STRING=Release ^
-    .. || goto :eof
+    .. || goto :error
 
 :: build
-cmake --build . --config Release || goto :eof
+cmake --build . --config Release || goto :error
 
 :: install
-cmake --build . --config Release --target install || goto :eof
+cmake --build . --config Release --target install || goto :error
 
 :: test - xmllint, diff and perl are required
 where xmllint || goto :eof
@@ -62,3 +56,6 @@ where diff || goto :eof
 perl --version || goto :eof
 where pdflatex.exe
 if %errorlevel% equ 0 ctest --output-on-failure -C Release
+
+:error
+exit /b 1
